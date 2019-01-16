1a20f2bfb878e4f958e16ce59f188464f542e2c4
大壮 <13793014727@163.com>
2019-01-16 14:38:51 +0800

    完成支付宝支付“

---
+++ app/src/main/java/com/bw/movie/presenter/SearchPresenter.java
@@ -0,0 +1,9 @@
+package com.bw.movie.presenter;
+
+/**
+ * date:2019/1/16
+ * author:李壮(大壮)
+ * function:
+ */
+public class SearchPresenter {
+}
---
+++ app/src/main/java/com/bw/movie/view/SearchView.java
@@ -0,0 +1,9 @@
+package com.bw.movie.view;
+
+/**
+ * date:2019/1/16
+ * author:李壮(大壮)
+ * function:
+ */
+public interface SearchView {
+}
